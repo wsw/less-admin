4b8dc9b51f4f03eb4c0609e0e2a95f1d0c321297
weishuwen <870224929@qq.com>
2019-06-19 18:01:38 +0800

    default页面

---
+++ components/hamburger/Hamburger.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    @click="toggleClick"
+    class="hamburger"
+    :class="{ 'is-active': isActive }"
+  >
+    <i class="el-icon-s-operation"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hamburger',
+  props: {
+    isActive: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    toggleClick() {
+      this.$emit('toggle')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hamburger {
+  display: inline-block;
+  margin-top: -5px;
+  vertical-align: middle;
+  cursor: pointer;
+  width: 20px;
+  height: 20px;
+  transform: rotate(0deg);
+  transition: 0.38s;
+  transform-origin: 50% 50%;
+}
+
+.hamburger.is-active {
+  transform: rotate(90deg);
+}
+.el-icon-s-operation {
+  color: white;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,56 +1,101 @@
 <template>
-  <div>
-    欢迎进入系统
-    <nuxt />
-  </div>
+  <el-container class="default-container">
+    <el-header class="header" height="45px">Header</el-header>
+    <el-container class="main-container">
+      <el-aside class="aside" :width="collapse ? '65px' : '200px'">
+        <el-menu
+          :collapse="collapse"
+          :collapse-transition="false"
+          background-color="#545c64"
+          text-color="#fff"
+          :router="true"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title"
+                >分组一</template
+              >
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title"
+                >选项4</template
+              >
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>
+        </el-menu>
+        <div class="aside-bottom">
+          <hamburger :is-active="collapse" @toggle="toggle"></hamburger>
+        </div>
+      </el-aside>
+      <el-main class="main"><nuxt /></el-main>
+    </el-container>
+  </el-container>
 </template>
 
+<script>
+import Hamburger from '@/components/hamburger/Hamburger'
+export default {
+  name: 'Default',
+  components: { Hamburger },
+  data() {
+    return {
+      collapse: false
+    }
+  },
+  methods: {
+    toggle() {
+      this.collapse = !this.collapse
+    }
+  }
+}
+</script>
+
+<style scoped>
+.default-container,
+.main-container {
+  height: 100%;
+}
+.header {
+  line-height: 45px;
+}
+.aside {
+  background: #545c64;
+  position: relative;
+  height: 100%;
+  padding-bottom: 40px;
+}
+.main {
+  height: 100%;
+}
+.aside-bottom {
+  text-align: center;
+  position: absolute;
+  bottom: 5px;
+  width: 100%;
+}
+</style>
+
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+html,
+body,
+#__nuxt,
+#__layout {
+  height: 100%;
+  width: 100%;
 }
 </style>
